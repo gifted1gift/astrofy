a68434f22b0ed443f598a4a939e28695a36c559f
gifted1gift <giftojochogwu@gmail.com>
2024-11-26 12:03:04 +0000

    Update config.ts
    meta title & meta description

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Godwin Gift |Digital Marketer    
-export const SITE_DESCRIPTION =A SEO specialist, product marketer and social media marketer with enough expertise to create visibility for brands.              
+export const SITE_TITLE = 'Godwin Gift | Digital Marketer';    
+export const SITE_DESCRIPTION = 'A SEO specialist, product marketer and social media marketer with enough expertise to create visibility for brands.';            
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
