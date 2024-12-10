367ff44ceec791c7ff945a8b87f07b226353337b
gifted1gift <giftojochogwu@gmail.com>
2024-12-10 15:33:29 -0800

    Update config.ts
    bag maker

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Godwin Gift | Digital Marketing Specialist';    
-export const SITE_DESCRIPTION = 'A SEO specialist, digital marketer and social media marketer with enough expertise to create visibility for brands.';            
+export const SITE_TITLE = 'Godwin Gift | Digital Marketing Specialist';Bag maker    
+export const SITE_DESCRIPTION = 'A SEO specialist, digital marketer, bag maker and social media marketer with enough expertise to create visibility for brands.';            
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
